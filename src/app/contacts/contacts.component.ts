a3ec15654b5f55eb3d51b4cc418d35a093fb5ffb
Joel Cannon <joelcannon@mac.com>
2024-05-03 14:26:33 -0500

    generate components

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css',
+})
+export class ContactsComponent {}
