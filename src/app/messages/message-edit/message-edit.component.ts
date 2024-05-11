b2ab2a8acfc07f7969c97de0ad5523f66e38c8ee
Joel Cannon <joelcannon@mac.com>
2024-05-10 20:46:45 -0500

    add message components

---
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-edit',
+  templateUrl: './message-edit.component.html',
+  styleUrl: './message-edit.component.css',
+})
+export class MessageEditComponent {}
---
+++ src/app/messages/message-item/message-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrl: './message-item.component.css',
+})
+export class MessageItemComponent {}
