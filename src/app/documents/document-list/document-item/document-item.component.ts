c09b74a293501fa3d9ec286cb5956c87f1a1904f
Joel Cannon <joelcannon@mac.com>
2024-05-10 20:32:39 -0500

    create document components

---
+++ src/app/documents/document-list/document-item/document-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-document-item',
+  templateUrl: './document-item.component.html',
+  styleUrl: './document-item.component.css',
+})
+export class DocumentItemComponent {}
---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-documents',
+  templateUrl: './documents.component.html',
+  styleUrl: './documents.component.css',
+})
+export class DocumentsComponent {}
