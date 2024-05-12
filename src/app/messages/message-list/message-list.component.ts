f2501ab83789d1a313ef7dc970d534d354f12726
Joel Cannon <joelcannon@mac.com>
2024-05-11 22:29:46 -0500

    Display and add messages to the MessageList component

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.css',
 })
-export class MessageListComponent {}
+export class MessageListComponent {
+  messages: Message[] = [
+    new Message(1, 'First Message', 'This is the first message', 'John Doe'),
+    new Message(2, 'Second Message', 'This is the second message', 'Jane Doe'),
+    new Message(3, 'Third Message', 'This is the third message', 'Jim Doe'),
+  ];
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
+}
