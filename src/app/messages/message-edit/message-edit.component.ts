5df81362d1022af7ee6cb4bfe77c3482e3684037
Joel Cannon <joelcannon@mac.com>
2024-05-11 22:19:19 -0500

    Modify the MessageEditComponent to send a new message

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Message } from '../message.model';
 @Component({
   selector: 'cms-message-edit',
   templateUrl: './message-edit.component.html',
   styleUrl: './message-edit.component.css',
 })
-export class MessageEditComponent {}
+export class MessageEditComponent {
+  @ViewChild('subject') subjectInputRef: ElementRef;
+  @ViewChild('msgText') msgTextInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Joel Cannon';
+
+  onSendMessage() {
+    const msgSubject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgTextInputRef.nativeElement.value;
+    const newMessage = new Message(1, msgSubject, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
+}
--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Message } from '../message-model';
+import { Message } from '../message.model';
 @Component({
   selector: 'cms-message-item',
   templateUrl: './message-item.component.html',
