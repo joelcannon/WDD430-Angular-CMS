d8126810b059b01cdec511e26ccc8e170c514f38
Joel Cannon <joelcannon@mac.com>
2024-05-15 21:56:25 -0500

    Implement the DocumentDetailComponent

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model'; // replace with the actual path to the Document model
 
 @Component({
   selector: 'cms-document-detail',
   templateUrl: './document-detail.component.html',
-  styleUrl: './document-detail.component.css',
+  styleUrls: ['./document-detail.component.css'],
 })
-export class DocumentDetailComponent {}
+export class DocumentDetailComponent {
+  @Input() document: Document;
+}
