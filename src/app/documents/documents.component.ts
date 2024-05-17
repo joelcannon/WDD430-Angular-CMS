6891511b137d0501aeefc81249fbeb13e4a49d79
Joel Cannon <joelcannon@mac.com>
2024-05-16 20:19:37 -0500

    Implement the DocumentsComponent

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Document } from './document.model';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
   styleUrl: './documents.component.css',
 })
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  selectedDocument: Document;
+}
