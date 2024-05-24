b50f1def8ce7a986af0f7e5a7ed73f5ed0eaa3da
Joel Cannon <joelcannon@mac.com>
2024-05-24 14:10:49 -0500

    implement documentService

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,51 +1,21 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css'],
 })
-export class DocumentListComponent {
-  @Output() selectedDocumentEvent = new EventEmitter<Document>();
-  documents: Document[]; // define the documents property
+export class DocumentListComponent implements OnInit {
+  documents: Document[] = [];
 
-  ngOnInit() {
-    console.log('DocumentListComponent initialized');
-    let doc1 = new Document(
-      1,
-      'Document 1',
-      'This is document 1',
-      'url-to-document-1',
-      []
-    );
-    let doc2 = new Document(
-      2,
-      'Document 2',
-      'This is document 2',
-      'url-to-document-2',
-      []
-    );
-    let doc3 = new Document(
-      3,
-      'Document 3',
-      'This is document 3',
-      'url-to-document-3',
-      []
-    );
-    let doc4 = new Document(
-      4,
-      'Document 4',
-      'This is document 4',
-      'url-to-document-4',
-      [doc1, doc2]
-    );
+  constructor(private documentService: DocumentService) {}
 
-    this.documents = [doc1, doc2, doc3, doc4];
-    console.log(this.documents);
+  ngOnInit(): void {
+    this.documents = this.documentService.getDocuments();
   }
-
   onSelectedDocument(document: Document) {
-    this.selectedDocumentEvent.emit(document);
+    this.documentService.documentSelectedEvent.emit(document);
   }
 }
---
+++ src/app/documents/document.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DocumentService {
+  documents: Document[] = [];
+  documentSelectedEvent = new EventEmitter<Document>();
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+  getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document {
+    return this.documents.find((document) => document.id === id) || null;
+  }
+}
