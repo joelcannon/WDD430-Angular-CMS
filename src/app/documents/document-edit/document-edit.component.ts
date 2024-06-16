4ec3aa795f944055917d927d936912967ccb2f8b
Joel Cannon <joelcannon@mac.com>
2024-06-15 19:21:35 -0500

    Implementing the Behavior of the Document Edit Form

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -3,27 +3,62 @@ import { NgForm } from '@angular/forms';
 
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
-
+import { Params, ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css',
 })
 export class DocumentEditComponent implements OnInit {
+  id: string;
   originalDocument: Document;
   document: Document;
-  editMode: boolean = false;
+  editMode = false;
 
-  ngOnInit(): void {
-    this.document = new Document('', '', '', '', []);
+  constructor(
+    private route: ActivatedRoute,
+    private documentService: DocumentService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.document = new Document('', '', '', '', []); // Initialize document here
+
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(this.id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
   }
 
   onSubmit(form: NgForm) {
-    // Handle form submission
-    console.log(form.value);
+    const value = form.value;
+    const newDocument = new Document(
+      value.id,
+      value.name,
+      value.description,
+      value.url,
+      null
+    );
+
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+
+    this.router.navigate(['/documents']);
   }
 
   onCancel() {
-    // Add your logic here
+    this.router.navigate(['/documents']);
   }
 }
